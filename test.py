71a893cbdd94556b5b6cd1b277c01e2aab05d905
Vladimir Leveryev <volodya.leveryev@gmail.com>
2023-08-18 23:28:29 +0900

    Исправление ошибки в расчёте WER

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os
+import re
 import sys
 from time import time
-from typing import Any
 
 from datasets import load_dataset, Audio, Dataset
 from evaluate import AutomaticSpeechRecognitionEvaluator
@@ -27,17 +27,26 @@ def prepare_pipeline(model_id: str, lang_id: str) -> None:
 
 
 def prepare_dataset(lang: str, split: str = "test") -> None:
+
+    def rm_spec_chars(batch):
+        col = "sentence"
+        chars = "[\,\?\.\!\-\;\:\"\“\%\‘\”\�\']"
+        batch[col] = re.sub(chars, "", batch[col]).lower()
+        return batch
+
     global dataset
     path = "mozilla-foundation/common_voice_13_0"
-    dataset = load_dataset(path, name=lang, split=split)
     datatype = Audio(sampling_rate=16000)
+    dataset = load_dataset(path, name=lang, split=split)
     dataset = dataset.cast_column("audio", datatype)
+    dataset = dataset.map(rm_spec_chars)
 
 
 def get_data_sample(sample_index: int = 0) -> ndarray:
     global dataset
     for index, data_item in enumerate(dataset):
         if index == sample_index:
+            # print(data_item["sentence"])
             return data_item["audio"]["array"]
 
 
@@ -46,16 +55,18 @@ def get_transcription(sample: ndarray) -> str:
     return output["text"]
 
 
-def get_evaluation() -> Any:
+def get_evaluation() -> (float, float):
     evaluator = AutomaticSpeechRecognitionEvaluator()
     evaluator.PIPELINE_KWARGS.pop("truncation")  # Грязный хак
-    result = evaluator.compute(
-        model_or_pipeline=pipe,
-        data=dataset,
-        metric="wer",
-        input_column="audio",
+    result_cer = evaluator.compute(
+        model_or_pipeline=pipe, data=dataset,
+        metric="cer", input_column="audio",
     )
-    return result["wer"]
+    result_wer = evaluator.compute(
+        model_or_pipeline=pipe, data=dataset,
+        metric="wer", input_column="audio",
+    )
+    return result_cer["cer"], result_wer["wer"]
 
 
 def main() -> None:
@@ -65,11 +76,6 @@ def main() -> None:
     print(f"Аутентификация в Hugging Face: {time() - before:.3f} с.")
 
     before = time()
-    # Базовая модель с 300 млн параметров для дообучения
-    # model_id = "facebook/mms-300m"
-    # Базовая модель с 1 млрд параметров для дообучения
-    # model_id = "facebook/mms-1b"
-    # Готовая модель с 1 млрд параметров и поддерживающая 1162 языка
     model_id = "facebook/mms-1b-all"
     model_lang = "sah"
 
@@ -78,7 +84,7 @@ def main() -> None:
 
     before = time()
     dataset_lang = "sah"
-    prepare_dataset(dataset_lang, "test[:10%]")
+    prepare_dataset(dataset_lang, "test")
     sample = get_data_sample()
     print(f"Загрузка набора данных: {time() - before:.3f} с.")
 
@@ -88,10 +94,10 @@ def main() -> None:
     duration = time() - before
     print(f"Вывод результата: {duration:.3f} с.")
 
-    print(f"Прогноз длительности расчета WER: {len(dataset) * duration:.3f} с.")
+    print(f"Прогноз времени расчета WER: {len(dataset) * duration * 2:.3f} с.")
     before = time()
-    evaluation = get_evaluation()
-    print(f"WER: {evaluation}")
+    cer, wer = get_evaluation()
+    print(f"CER: {cer:.3f}, WER: {wer:.3f}")
     print(f"Расчет WER: {time() - before:.3f} с.")
 
 
